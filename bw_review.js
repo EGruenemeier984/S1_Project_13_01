492f5028a251f2f42435ac60c648d63e097ac997
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-03-13 15:23:06 -0700

    Added init function
    Used object collection to set attributes using JavaScript.

--- bw_review.js
+++ bw_review.js
@@ -33,19 +33,36 @@
       within textStr
 
 */
+window.onload = init;
+
+function init() {
+    var stars = document.querySelectorAll("span#stars img");
+
+    for (var i = 0; i < stars; i++) {
+        stars[i].style.cursor = pointer;
+        stars[i].addEventListener("mouseenter", lightStars);
+    }
+    document.getElementById("comment").addEventListener("keyup", updateCount);
+}
+
+function lightStars(e) {
+    var starNumber = document.getElementById("stars").alt;
+    var stars = document.querySelectorAll("span#stars img");
+    for (var i = 0; i < starNumber; i++) {
+        stars[i].src = "bw_stars2.png";
+    }
+}
 
 
 
-  
 
 
-  
-  
-  
+
+
 /*=================================================================*/
 
 function countCharacters(textStr) {
-   var commentregx = /\s/g;
-   var chars = textStr.replace(commentregx, "");
-   return chars.length;
-}   
+    var commentregx = /\s/g;
+    var chars = textStr.replace(commentregx, "");
+    return chars.length;
+}
