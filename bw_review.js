e9529928eed197a68727de85e1e36b756ff24581
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-03-14 14:31:32 -0700

    Final Commit
    Finished creating the star rating system along with creating a word count and a way to let he user know that they have exceeded the max amount of words. This project is ready for review.

--- bw_review.js
+++ bw_review.js
@@ -33,31 +33,59 @@
       within textStr
 
 */
-window.onload = init;
-
+// This loads the init function as the window loads.
+window.onload = init();
+// This function intializes the stars as the user hover over them and creates event listeners so more function can thake place.
 function init() {
     var stars = document.querySelectorAll("span#stars img");
 
-    for (var i = 0; i < stars; i++) {
-        stars[i].style.cursor = pointer;
+    for (var i = 0; i < stars.length; i++) {
+        stars[i].style.cursor = "pointer";
         stars[i].addEventListener("mouseenter", lightStars);
     }
     document.getElementById("comment").addEventListener("keyup", updateCount);
 }
-
-function lightStars(e) {
-    var starNumber = document.getElementById("stars").alt;
+// This function ligts up the stars as they are hovered over and when they are ckicked the number is saved and displayed.
+function lightStars() {
+    var starNumber = event.target.alt;
     var stars = document.querySelectorAll("span#stars img");
     for (var i = 0; i < starNumber; i++) {
-        stars[i].src = "bw_stars2.png";
+        stars[i].src = "bw_star2.png";
     }
+    for (var j = starNumber; j < 5; j++) {
+        stars[j].src = "bw_star.png"
+    }
+    document.getElementById("rating").value = starNumber + " stars";
+    event.target.addEventListener("mouseleave", turnOffStars);
+    event.target.addEventListener("click",
+        function () {
+            event.target.removeEventListener("mouseleave", turnOffStars);
+        }
+    );
 }
+// This function turns the stars to an image that isn't lighten up.
+function turnOffStars() {
+    var stars = document.querySelectorAll("span#stars img");
+    for (var i = 0; i < stars.length; i++) {
+        stars[i].src = "bw_star.png";
+    }
+    document.getElementById("rating").value = "";
+}
+// This function updates the word count for the textarea along with chaging the background and font color if the word count exceeds 1000 words.
+function updateCount() {
+    var commentText = document.getElementById("comment").value;
+    var charCount = countCharacters(commentText);
+    var wordCountBox = document.getElementById("wordCount");
+    wordCountBox.value = charCount + "/1000";
+    if (charCount > 1000) {
+        wordCountBox.style.backgroundColor = "red";
+        wordCountBox.style.color = "white";
+    } else {
+        wordCountBox.style.backgroundColor = "white";
+        wordCountBox.style.color = "black";
+    }
 
-
-
-
-
-
+}
 
 /*=================================================================*/
 
